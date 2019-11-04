32e2e9acfcb1797c4185b513837905fe542fda16
Jim Livsey <james.a.livsey@gmail.com>
2019-11-04 13:06:32 -0500

    Update setup.R

--- worksheets/worksheet-answers/Rcode/setup.R
+++ worksheets/worksheet-answers/Rcode/setup.R
@@ -1,27 +1,31 @@
 library(seasonal)
 
-fsdata <- import.ts(file = "//it171oafs-oa17/ECON_SHARE/X-13/courses/2019CR/data/retail/furniture stores.dat",
-                    format = "datevalue",
-                    name = "James-furniture")
-
-fs <- seas(fsdata, series.span="1998.1,", x11="")
-
-out(fs)
-
-view(fs)
-
-
-
-dat <- import.ts(file = "//it171oafs-oa17/ECON_SHARE/X-13/courses/2019CR/data/cr/CR Regional IMAE.dat",
+bookdata <- import.ts(file = "C:\\Users\\livse301\\Documents\\GitHub\\CR-SAclass\\data\\retail\\Book stores.dat",
                     format = "datevalue")
-plot(dat)
-spec.ar(dat)
-spec.pgram(dat)
 
-acf(diff(dat))
+plot(bookdata)
 
-n <- length(dat)
-M <- matrix(dat[-((n-7):n)], ncol = 12)
-plot.ts(M[, 1:4])
+m <- seas(bookdata, x11="")
+
+out(m)
+
+library(seasonalview)
+view(m)
+
+sa <- final(m)
+plot(sa)
+
+tr <- trend(m)
+
+plot(bookdata)
+lines(sa, col = 2)
+lines(tr, col = 3)
+
+summary(m)
+udg(m)
+
+udg(m, "nfcst")
+
+vignette("seas")
 
 
